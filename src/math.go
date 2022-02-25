0490ad0249bad41674304b230ff299393dfd4597
“RossiniM” <rossini.miranda@mercadolivre.com>
2022-02-25 18:13:53 -0300

    feat: include multiply operation

--- src/math.go
+++ src/math.go
@@ -6,7 +6,14 @@ func Sum(n1 int, n2 int) int {
 	return n1 + n2
 }
 
+func Multiply(n1 int, n2 int) int {
+	return n1 * n2
+}
+
 func main() {
 
-	fmt.Print("asssasasasssass")
+	fmt.Print(Sum(1, 3))
+	fmt.Print("\n")
+
+	fmt.Print(Multiply(1, 3))
 }
